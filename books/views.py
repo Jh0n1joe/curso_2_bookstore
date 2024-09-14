8ee06de56687ead5af7d4e7e68bf111a2fd8c9bf
Jhony.j <jhonycrq1816@gmail.com>
2024-09-14 15:28:09 -0400

    books-login/details

--- books/views.py
+++ books/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Book
 
 class BookListView(ListView):
@@ -13,6 +14,8 @@ class BookListView(ListView):
             return Book.objects.filter(title__icontains=query)  # Filtrar libros por título
         return Book.objects.all()  # Si no hay búsqueda, mostrar todos los libros
 
-class BookDetailView(DetailView):
+
+class BookDetailView(LoginRequiredMixin, DetailView):
     model = Book
     template_name = 'books/book_detail.html'
+    login_url = '/accounts/login/'  # URL a la que redirigir si no está autenticado
